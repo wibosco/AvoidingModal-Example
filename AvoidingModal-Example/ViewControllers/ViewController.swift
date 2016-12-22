ffe8a28254019a39a2b279b209e0d527f27038e8
William Boles <william@memrise.com>
2016-12-22 15:56:29 +0000

    Empty xcode project

---
+++ AvoidingModal-Example/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvoidingModal-Example
+//
+//  Created by William Boles on 22/12/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
